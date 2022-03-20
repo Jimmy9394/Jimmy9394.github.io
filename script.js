a57d17c7236e64e073b23714cf96a3c66f98c41a
Jimmy <happy69good@gmail.com>
2022-03-20 19:12:46 +0800

    update sytles

--- script.js
+++ script.js
@@ -1,88 +1,76 @@
-function gogo()
-{
-  console.log("gogo");  
-  window.location.replace("./question1.html");}
-
-  function question1RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：F"; 
-  alert(hint); 
-  window.location.replace("./finish.html");
-}
-  function question1WrongAnswer()
-{
-  console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+function gogo() {
+  console.log("gogo");
+  window.location.replace("./question1.html");
 }
 
-function question2RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：a"; 
-  alert(hint); 
+function question1RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：F";
+  alert(hint);
   window.location.replace("./finish.html");
 }
-function question2WrongAnswer()
-{
+function question1WrongAnswer() {
   console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
 }
 
-function question3RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：m"; 
-  alert(hint); 
+function question2RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：a";
+  alert(hint);
   window.location.replace("./finish.html");
 }
-function question3WrongAnswer()
-{
+function question2WrongAnswer() {
   console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
 }
 
-function question4RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：i"; 
-  alert(hint); 
+function question3RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：m";
+  alert(hint);
   window.location.replace("./finish.html");
 }
-function question4WrongAnswer()
-{
+function question3WrongAnswer() {
   console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
 }
 
-function question5RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：l"; 
-  alert(hint); 
+function question4RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：i";
+  alert(hint);
   window.location.replace("./finish.html");
 }
-function question5WrongAnswer()
-{
+function question4WrongAnswer() {
   console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
 }
 
-function question6RightAnswer()
-{
-  console.log("you are rightt!!!!!!"); 
-  let hint = "提示：y"; 
-  alert(hint); 
+function question5RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：l";
+  alert(hint);
   window.location.replace("./finish.html");
 }
-function question6WrongAnswer()
-{
+function question5WrongAnswer() {
   console.log("you are wrong!!!!!");
-  let hint1 = "這麼簡單也答錯"; 
-  alert(hint1); 
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
+}
+
+function question6RightAnswer() {
+  console.log("you are rightt!!!!!!");
+  let hint = "提示：y";
+  alert(hint);
+  window.location.replace("./finish.html");
+}
+function question6WrongAnswer() {
+  console.log("you are wrong!!!!!");
+  let hint1 = "這麼簡單也答錯";
+  alert(hint1);
 }
